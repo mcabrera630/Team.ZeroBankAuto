760a4a37866c882cde155ac39298dbbaa54bfff8
Mauricio Cabrera <mcabrera630@gmail.com>
2019-12-24 19:59:29 -0500

    Added Hooks

--- src/test/java/com/zerobank/stepdefinitions/Hooks.java
+++ src/test/java/com/zerobank/stepdefinitions/Hooks.java
@@ -1,4 +1,18 @@
 package com.zerobank.stepdefinitions;
+import com.zerobank.utilities.Driver;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
 
 public class Hooks {
+    WebDriver driver=Driver.get();
+    @Before
+    public void setUp(){
+        System.out.println("This is before method...");
+    }
+    @After
+    public void tearDown(){
+        driver.close();
+    }
+
 }
